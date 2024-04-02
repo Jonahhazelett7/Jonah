6ac38ed1987bfb6f93e48973ff75d85ee9c9c601
Jonahhazelett7 <157169042+Jonahhazelett7@users.noreply.github.com>
2024-04-01 20:19:01 -0400

    Initial commit

---
+++ Homework6/data_prep.py
@@ -0,0 +1,28 @@
+import sys
+sys.path.append('../')
+
+from utilities import ChatTemplate
+import os
+
+# for honmework 6, please change both values below based on the hw instructions
+folder_name = "cinema"
+output_file = "cinema.jsonl"
+
+template = ChatTemplate.from_file('jsonl.json')
+jsonl = ''
+
+for f in os.listdir(folder_name):
+    # Open each file in read mode with utf-8 encoding, ignoring errors
+    with open(os.path.join(folder_name, f), 'r', encoding="utf-8", errors="ignore") as f:
+        # Read the content of the file and store it in the variable `text`
+        text = f.read()
+
+    # Pass the text content to a template for processing and store the response
+    #   each completion makes 20 prompts
+    response = template.completion({'info': text, 'n': '20'})
+    # Extract the processed text from the response, 
+    #   remove leading/trailing whitespace, and append it to a JSONL string
+    jsonl += response.choices[0].message.content.strip() + '\n'
+
+with open(output_file, 'w+') as f:
+    f.write(jsonl)
---
+++ Homework6/fine_tuned_model.py
@@ -0,0 +1,28 @@
+import openai
+import os
+
+openai.api_key = os.environ['OPENAI_API_KEY']
+
+# for homework 6, please change this to your own model
+model_name = 'ft:gpt-3.5-turbo-1106:personal::99LPYTTC'
+
+template = '''You are a Q&A bot. You provide short answers to questions.
+For example:
+Question: What does ASL stand for? American Sign Language.
+Provide the answer to the following question:
+Question: '''
+
+while True:
+    prompt = input('user: ')
+    if prompt == 'exit':
+        break
+
+    message = [{'role': 'user', 'content': template + prompt}]
+
+    response = openai.chat.completions.create(
+        model = model_name,
+        temperature = 0,
+        stop = ['\n'],
+        messages = message)
+
+    print("assistant: " + response.choices[0].message.content)
